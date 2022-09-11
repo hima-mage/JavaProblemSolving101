6bfef9205abe4059ecf88abd18a349a2c115102b
ishiekh-dev <ishiekh.dev.v1@gmail.com>
2022-09-11 04:11:43 +0200

    add Max_value and min condition to check boundaries of the intger

--- PackageA/06.ReversDigitNumber.java
+++ PackageA/06.ReversDigitNumber.java
@@ -17,17 +17,23 @@ package  PackageA;
 
         while (num != 0 ) {
             // get last element and asign it to reverse + reverse * 10 cuz this means
-            reverse = reverse*10 +  num%10;
-            num = num / 10; // remove last digit from the num
+            if(reverse > Integer.MAX_VALUE/10 || reverse == Integer.MAX_VALUE && num%10 < 7) {
+                return  0;
+            }
+            if(reverse < Integer.MIN_VALUE/10 || reverse == Integer.MIN_VALUE && num%10 < -8) {
+                return  0;
+            }
+            reverse = (reverse*10) +  (num%10);
+            num /=  10; // remove last digit from the num
 
         }
-
         return reverse;
     }
 
     public static void main(String[] args) {
 
-        System.out.println(reverseNumber(1234567));
-        System.out.println(reverseNumber(7654321));
+//        System.out.println(reverseNumber(1234567));
+//        System.out.println(reverseNumber(7654321));
+        System.out.println(reverseNumber(1534236469));
     }
 }
